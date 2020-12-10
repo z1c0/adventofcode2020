f8fa313260f66a5d52ad799a9497e0b551862eb1
wolfgang.ziegler <wolfgang.ziegler@dynatrace.com>
2020-12-10 22:15:07 +0100

    Day 10

---
+++ Day10/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Day9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("START");
+            Part1();
+            Part2();
+            System.Console.WriteLine("DONE");
+        }
+
+        private static void Part1()
+        {
+            var numbers = ReadInput();
+            var diff1 = 0;
+            var diff3 = 0;
+            for (var i = 1; i < numbers.Count; i++)
+            {
+                var diff = numbers[i] - numbers[i - 1];
+                if (diff == 1)
+                {
+                    diff1++;
+                }
+                if (diff == 3)
+                {
+                    diff3++;
+                }
+            }
+            System.Console.WriteLine($"diff1: {diff1}, diff3: {diff3} - diff1 * diff3 = {diff1 * diff3}");
+        }
+
+        private static void Part2()
+        {
+            var numbers = ReadInput();
+            var cache = new Dictionary<int, long>();
+            var combinations = TryList(numbers, 0, cache);
+            System.Console.WriteLine($"combinations: {combinations}");
+        }
+
+        private static long TryList(List<int> numbers, int startPos, Dictionary<int, long> cache)
+        {
+            if (cache.ContainsKey(startPos))
+            {
+                return cache[startPos];
+            }
+            if (startPos == numbers.Count - 1)
+            {
+                return 1;
+            }
+            long combinations = 0;
+            for (var i = startPos + 1; i < numbers.Count; i++)
+            {
+                if (numbers[i] - numbers[startPos] <= 3)
+                {
+                    combinations += TryList(numbers, i, cache);
+                }
+            }
+            cache[startPos] = combinations;
+            return combinations;
+        }
+
+        private static List<int> ReadInput()
+        {
+            var numbers = new List<int>();
+            var lines = File.ReadAllLines("input.txt");
+            foreach (var line in lines)
+            {
+                numbers.Add(Int32.Parse(line));
+            }
+            numbers.Add(0);
+            numbers.Sort();
+            numbers.Add(numbers.Last() + 3);
+            return numbers;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # Advent of Code 2020
 
 https://adventofcode.com/2020
+
+[Day 10](https://adventofcode.com/2020/day/10) was really hard for me.
+I found a very cumbersome solution with exponential runtime.
+I head to cheat a little bit and check for an answer on the internet and found
+this impressive solution on  [YouTube](https://www.youtube.com/watch?v=cE88K2kFZn0).
+The answer was to use recursion and dynamic programming (memoization) to get the
+complexity down to O(n*n).
