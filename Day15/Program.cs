bd5f7a29d425d51db3198c7a6ed91002f5fe63d7
wolfgang.ziegler <wolfgang.ziegler@dynatrace.com>
2020-12-15 18:01:46 +0100

    Day 15

---
+++ Day15/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Day15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("START");
+            Solve(2020);
+            Solve(30000000);
+            System.Console.WriteLine("DONE");
+        }
+
+		private static void Solve(int maxRound)
+        {
+            var numbers = ReadInput();
+            var history = new Dictionary<int, Tuple<int, int>>();
+            var round = 1;
+            var lastSpoken = 0;
+            foreach (var n in numbers)
+            {
+                history[n] = new Tuple<int, int>(round++, 0);
+                lastSpoken = n;
+            }
+            while (round <= maxRound)
+            {
+                if (history.TryGetValue(lastSpoken, out var value))
+                {
+                    if (value.Item2 == 0) // first time?
+                    {
+                        lastSpoken = 0;
+                        Remember(history, lastSpoken, round);
+                    }
+                    else
+                    {
+                        lastSpoken = value.Item1 - value.Item2;
+                        Remember(history, lastSpoken, round);
+                    }
+                }
+                round++;
+            }
+            System.Console.WriteLine($"Last number spoken after {maxRound} rounds is {lastSpoken}" );
+        }
+
+        private static void Remember(Dictionary<int, Tuple<int, int>> dictionary, int number, int round)
+        {
+            if (dictionary.TryGetValue(number, out var value))
+            {
+                dictionary[number] = new Tuple<int, int>(round, value.Item1);
+            }
+            else
+            {
+                dictionary.Add(number, new Tuple<int, int>(round, 0));
+            }
+        }
+
+        private static List<int> ReadInput()
+        {
+            var line = File.ReadAllText("input.txt");
+            var tokens = line.Split(",");
+            var numbers = new List<int>();
+            foreach (var t in tokens)
+            {
+                numbers.Add(int.Parse(t));
+            }
+            return numbers;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -9,7 +9,7 @@ this impressive solution on  [YouTube](https://www.youtube.com/watch?v=cE88K2kFZ
 The answer was to use recursion and dynamic programming (memoization) to get the
 complexity down to O(n*n).
 
-The second part of [Day 13](https://adventofcode.com/2020) was also too tough for me.
+The second part of [Day 13](https://adventofcode.com/2020/day/13) was also too tough for me.
 I found a solution for the simple examples, but for the puzzle input the runtime
 was too bad I my program would not finish.
 After researching the issue on the internet, I found solutions mentioning the
@@ -18,3 +18,8 @@ which is probably the correct number theory approach. I did not bother to unders
 that in detail yet to be honest since I also found this very compact and elagant
 [solution](https://github.com/Chrinkus/advent-of-code-2020/blob/main/src/day13.cpp)
 that I "borrowed" for my solution then.
+
+[Day 15](https://adventofcode.com/2020/day/15) was surprisingly easy after the
+puzzles had quite increased in complexity over the last couple of days.
+My solution is probably not ideal and the second part ran for quite a while
+(~ 30 seconds) but still it terminated and yielded the correct solution.
